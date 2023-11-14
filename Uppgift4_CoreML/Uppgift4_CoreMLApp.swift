e574bbd1390b856b6146294b1e574e4158b10795
Alexander Lins <alexanderlins88@icloud.com>
2023-11-14 10:11:03 +0100

    Initial Commit

---
+++ Uppgift4_CoreML/Uppgift4_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift4_CoreMLApp.swift
+//  Uppgift4_CoreML
+//
+//  Created by Alexander Lins on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift4_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
